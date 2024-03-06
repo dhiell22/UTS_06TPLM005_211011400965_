2285de3751c4b4de132230aafd59e7ac4e1c2641
dhiell22 <154538448+dhiell22@users.noreply.github.com>
2024-03-06 20:25:16 +0700

    Add files via upload

---
+++ Fadilpert1.php
@@ -0,0 +1,25 @@
+<?php 
+print"<pre>";
+print "<center><font size=14 color='magenta'>BIODATA</font></center>";
+echo "<hr size=4 width=60% color='blue'>";
+print "<center><font size=12 color='red'> Nama      : Fadil Ramadhan </font><br></center>";
+print "<center><font size=12 color='red'> Email     : fadilmadhon22@gmail.com </font><br></center>";
+print "<center><font size=12 color='red'> Jurusan   : Teknik Informatika </font><br></center>";
+print "<center><font size=12 color='red'> Hobby     : Healing </font><br></pre></center>";
+echo "<hr size=4 width=60% color='blue'>";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
